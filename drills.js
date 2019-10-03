be58fb01bfb171c9fb070c37aae1203602349e97
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-03 14:41:48 -0400

    addBefore, addAfter, addAt

--- drills.js
+++ drills.js
@@ -1,8 +1,39 @@
 'use strict';
 const LinkedList = require('./list');
 
-let LL = new LinkedList();
+let SLL = new LinkedList();
 
-LL.insertFirst('ABC');
-LL.insertLast('DEF');
-console.log(LL.head);
+SLL.insertLast('Apollo');
+SLL.insertLast('Boomer');
+SLL.insertLast('Helo');
+SLL.insertLast('Husker');
+SLL.insertLast('Starbuck');
+
+console.log(SLL.head);
+console.log(SLL.head.next);
+console.log(SLL.head.next.next);
+console.log(SLL.head.next.next.next);
+console.log(SLL.head.next.next.next.next);
+
+SLL.insertLast('Tauhida');
+console.log(SLL.head.next.next.next.next.next);
+
+SLL.remove('squirrel');
+
+SLL.insertBefore('Athena', 'Boomer');
+console.log(SLL.head);
+console.log(SLL.head.next);
+console.log(SLL.head.next.next);
+
+SLL.insertAfter('Hotdog', 'Helo');
+console.log(SLL.head.next.next);
+console.log(SLL.head.next.next.next);
+console.log(SLL.head.next.next.next.next);
+
+SLL.insertAt('Kat', 2);
+console.log(SLL.head);
+console.log(SLL.head.next);
+console.log(SLL.head.next.next);
+console.log(SLL.head.next.next.next);
+
+SLL.remove('Tauhida');
--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 'use strict';
 class _Node {
-  constructor (data, next) {
-    this.data = data;
+  constructor (value, next) {
+    this.value = value;
     this.next = next;
   }
 }
@@ -11,24 +11,94 @@ class LinkedList {
     this.head = null;
   }
 
-  insertFirst (data) {
-    this.head = new _Node(data, this.head);
+  insertFirst (value) {
+    this.head = new _Node(value, this.head);
   }
 
-  insertLast (data) {
-    if (this.head === null) this.insertFirst(data);
+  insertLast (value) {
+    if (this.head === null) this.insertFirst(value);
     else {
-      let crawler = this.head;
-      while (crawler.next !== null) {
-        crawler = crawler.next;
+      let currNode = this.head;
+      while (currNode.next !== null) {
+        currNode = currNode.next;
       }
-      crawler.next = new _Node(data, null);
+      currNode.next = new _Node(value, null);
     }
   }
 
-  remove () {}
+  remove (item) {
+    if (!this.head) {
+      return null;
+    }
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    } else {
+      let prevNode = this.head;
+      let currNode = this.head;
+      while (currNode.next !== null && currNode.value !== item) {
+        prevNode = currNode;
+        currNode = currNode.next;
+      }
+      if (currNode.value === item) prevNode.next = currNode.next;
+      else console.log('item not found!');
+    }
+  }
 
-  find () {}
+  find (item) {
+    if (this.head === null) return null;
+    let currNode = this.head;
+    while (currNode.next !== null) {
+      if (currNode.value === item) return currNode;
+      currNode = currNode.next;
+    }
+    return null;
+  }
+
+  insertBefore (newData, key) {
+    if (this.head === null || this.head.value === key) this.insertFirst(newData);
+    else {
+      let currNode = this.head;
+      while (currNode.next !== null) {
+        if (currNode.next.value === key) {
+          currNode.next = new _Node(newData, currNode.next);
+          return currNode.next;
+        }
+        currNode = currNode.next;
+      }
+      currNode.next = new _Node(newData, null);
+      return currNode.next;
+    }    
+  }
+
+  insertAfter (newData, key) {
+    if (this.head === null) this.insertFirst(newData);
+    else {
+      let currNode = this.head;
+      while (currNode.next !== null) {
+        if (currNode.value === key) {
+          currNode.next = new _Node(newData, currNode.next);
+          return currNode.next;
+        }
+        currNode = currNode.next;
+      }
+      currNode.next = new _Node(newData, null);
+      return currNode.next;
+    }    
+  }
+
+  insertAt (newData, index) {
+    if (this.head === null || index === 0) this.insertFirst(newData);
+    else {
+      let i=0;
+      let currNode=this.head;
+      while (i< index-1 && currNode.next !== null) {
+        i++;
+        currNode = currNode.next;
+      }
+      currNode.next = new _Node(newData, currNode.next);
+    }
+  }
 }
 
 module.exports = LinkedList;
